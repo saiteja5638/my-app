963d89b9a7ed1ba47bb380dc33fe6b545c18259b
Spark Sai teja <sparksaiteja@gmail.com>
2025-03-07 09:26:35 +0000

    new commity

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Home from './components/pages/Home';
-import Reveal from './components/pages/Reveal';
-
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home></Home>
-  },
-  {
-    path:'/reveal',
-    element : <Reveal></Reveal>
-  }
-
-])
 
 function App() {
   return (
     <>
       <main>
-        <RouterProvider router={router}></RouterProvider>
+      <div className="full-screen-background" >
+
+</div>
       </main>
     </>
   );
